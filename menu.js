0d1b6935674979e579335def9a4a56cec1e6848e
Fredy <142295949+fredrickomondiodongo@users.noreply.github.com>
2023-09-27 10:22:57 +0300

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,13 @@
+function opennav(){
+    document.getElementById("sideNav").style.width="200px"
+    document.getElementById("menuBtn").style.display="none"
+    document.getElementById("closeBtn").style.right= "30px"
+}
+
+function closenav(){
+    document.getElementById("sideNav").style.width="0px"
+    document.getElementById("menuBtn").style.display="block"
+    document.getElementById("closeBtn").style.right= "-55px"  
+}
+
+src="https://cdn.jsdelivr.net/npm/smooth-scroll@16.1.3/dist/smooth-scroll.polyfills.min.js"
